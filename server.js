056c1b4efc9d260fd0d0e2b8949e9ecba40df0ce
yuvalbl <yuval.bar.levi@gmail.com>
2019-09-08 19:42:23 +0300

    update whitelist

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const corsOptions = {
     origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
