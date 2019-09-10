206720682da70a131bf1c4429095656847ac0b61
yuvalbl <yuval.bar.levi@gmail.com>
2019-09-10 14:43:08 +0300

    add tests to server

---
+++ quotes.service.test.js
@@ -0,0 +1,18 @@
+'use strict';
+const quotesService = require('./quotes.service');
+
+test('getRandomQuote should return a quote with body and source', () => {
+    const quote = quotesService.getRandomQuote();
+
+    expect(quote.body).toBeDefined();
+    expect(quote.source).toBeDefined();
+    expect(typeof quote.body).toMatch('string');
+    expect(typeof quote.source).toMatch('string');
+});
+
+
+test('getRandomQuote should return a quote with body length greater than 9', () => {
+    const quote = quotesService.getRandomQuote();
+
+    expect(quote.body.length).toBeGreaterThan(9);
+});
