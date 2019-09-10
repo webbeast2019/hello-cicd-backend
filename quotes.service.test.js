3ce34a08d90d008753f54c9765b779db9aadc0f2
yuvalbl <yuval.bar.levi@gmail.com>
2019-09-10 17:57:13 +0300

    improve tests

--- quotes.service.test.js
+++ quotes.service.test.js
@@ -4,12 +4,11 @@ const quotesService = require('./quotes.service');
 test('getRandomQuote should return a quote with body and source', () => {
     const quote = quotesService.getRandomQuote();
 
-    expect(quote.body).toBeDefined();
-    expect(quote.source).toBeDefined();
+    expect(quote).toBeDefined();
+    expect(typeof quote).toBeObject('object');
     expect(typeof quote.body).toMatch('string');
     expect(typeof quote.source).toMatch('string');
 });
-
 
 test('getRandomQuote should return a quote with body length greater than 9', () => {
     const quote = quotesService.getRandomQuote();
